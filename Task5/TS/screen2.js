7c18503e07873986ce9bb24672ec0497f7e880bd
vedantvasaikarzeus <vedant.vasaikar@zeuslearning.com>
2025-06-13 12:25:43 +0530

    'tsdone'

---
+++ Task5/TS/screen2.js
@@ -0,0 +1,258 @@
+function populateCard(cardDataArray) {
+    var section = document.querySelector('.container');
+    if (!section) {
+        return "<h1> nothing</h1>";
+    }
+    cardDataArray.forEach(function (cardData) {
+        var card = document.createElement('section');
+        card.className = 'section3';
+        card.innerHTML = "\n            ".concat(cardData.isExpired ? "<div class=\"expired\"><p>EXPIRED</p></div>" : '', "\n            <div class='card'>\n                \n                <div class=\"courseimage\">\n                    <img src=\"").concat(cardData.img, "\">\n                </div>\n                <div class=\"coursedetails\">\n                    <div class=\"sec1\">\n                        <p>").concat(cardData.topic, "</p>\n                        ").concat(cardData.is_favourite ? "<img src=\"./icons/favourite.svg\">" : "<img src=\"./icons/notFavourite.svg\">", "\n                        \n                    </div>\n                    <div class=\"sec2\">\n                        <p>").concat(cardData.subject, "</p>\n                        <p>|</p>\n                        <p>Grade<span> ").concat(cardData.grade, "</span><span style=\"color : green;\"> ").concat(cardData.grade_plus, "</span></p>\n                    </div>\n                    <div class=\"sec3\">\n                    ").concat(cardData.units ? "<p><span><b>".concat(cardData.units, "</b></span> Units</p>") : '', "\n                    ").concat(cardData.lessons ? "<p><span><b>".concat(cardData.lessons, "</b></span> Lessons</p>") : '', "\n                    ").concat(cardData.topics ? "<p><span><b>".concat(cardData.topics, "</b></span> Topics</p>") : '', "\n                    </div>\n\n                    <div class=\"sec4\">\n                        <select class=\"teachername\" id=\"teachername\">\n                        \n                            ").concat(cardData.teacher_class ? "<option  selected>".concat(cardData.teacher_class, "</option>") : "<option  selected>No Classes</option>", "\n                        </select>\n                    </div>\n                    <div class=\"sec5\">\n                        ").concat(cardData.no_of_students ? "<p>".concat(cardData.no_of_students, " Students</p>") : '', "\n                        ").concat(cardData.date_of_class ? "<p>|</p>" : '', "\n                        ").concat(cardData.date_of_class ? "<p>".concat(cardData.date_of_class, "</p>") : '', "\n                    </div>\n                </div>\n            </div>\n            \n        ");
+        var actions = document.createElement('div');
+        actions.className = 'actions';
+        actions.innerHTML = "\n            <div class='div1' style=\"opacity: ".concat(cardData.preview ? "1" : "0.4", "\">\n                <img src=\"./icons/preview.svg\">\n            </div>\n            <div  style=\"opacity: ").concat(cardData.manage_course ? "1" : "0.4", "\">\n                <img src=\"./icons/manage course.svg\">\n            </div>\n            <div  style=\"opacity: ").concat(cardData.grade_submission ? "1" : "0.4", "\">\n                <img src=\"./icons/grade submissions.svg\">\n            </div>\n            <div class='div4' style=\"opacity: ").concat(cardData.reports ? "1" : "0.4", "\">\n                <img src=\"./icons/reports.svg\">\n            </div>\n        ");
+        section === null || section === void 0 ? void 0 : section.appendChild(card);
+        card.appendChild(actions);
+    });
+}
+function populateannouncement(annDataArray) {
+    var section = document.querySelector('.announcementitems');
+    if (!section) {
+        return "<h1> no section</h1>";
+    }
+    annDataArray.forEach(function (annData) {
+        var annitem = document.createElement('div');
+        annitem.className = 'annitem'; // Fixed variable name from 'item' to 'annitem'
+        annitem.style.backgroundColor = !annData.checked ? "#FFFFEE" : 'white';
+        annitem.innerHTML = "\n            <div class=\"annsec1\">\n                <div class=\"panadname\">\n                    <span class=\"pa\">PA:</span><span class=\"name\">".concat(annData.pa, "</span>\n                </div>\n                <div class=\"checkimage\">\n                    ").concat(annData.checked ? "<img src=\"./icons/checkbox-checked.svg\">" : "<img src=\"./icons/checkbox-unchecked.svg\">", "\n                </div>\n            </div>\n            <div class=\"annsec2\">\n                <div class=\"content\">\n                    <p>").concat(annData.content, "</p>\n                </div>\n            </div>\n            <div class=\"annsec3\">\n                <div class=\"courseinann\">\n                    ").concat(!annData.course ? '' : "<span>Course :</span> <span class=\"coursename\">".concat(annData.course, "</span>"), "\n                    \n                </div>\n            </div>\n            <div class=\"annsec4\">\n                <div class=\"files\">\n                    ").concat(annData.filesAttached != 0 ? "<span>".concat(annData.filesAttached, "</span><span> Files attached</span>") : '', "\n                    \n                </div>\n                <div class=\"dateandtime\">\n                    ").concat(annData.dateAndTime, "\n                </div>\n            </div>\n        ");
+        section.appendChild(annitem);
+    });
+}
+function populatebelll(bellDataArray) {
+    var section = document.querySelector('.bellitems');
+    if (!section) {
+        return "<h1>no data to show</h1>";
+    }
+    bellDataArray.forEach(function (bellData) {
+        var bellitem = document.createElement('div');
+        bellitem.className = 'bellitem'; // Fixed variable name from 'item' to 'bellitem'
+        bellitem.style.backgroundColor = !bellData.checked ? "#FFFFEE" : 'white';
+        bellitem.innerHTML = "\n                \n                    <div class=\"bellsec1\">\n                        <div class=\"bellcontent\">\n                            <p>".concat(bellData.content, "</p>\n                        </div>\n                        <div class=\"bellcheckimage\">\n                            ").concat(bellData.checked ? "<img src=\"./icons/checkbox-checked.svg\">" : "<img src=\"./icons/minus.png\">", "\n                        </div>\n                    </div>\n                    <div class=\"bellsec2\">\n                        <div class=\"bellcourse\">\n                            ").concat(!bellData.course ? '' : "<span>Course :</span> <span class=\"coursename\">".concat(bellData.course, "</span>"), "\n                        </div>\n                    </div>\n                    <div class=\"bellsec3\">\n                        <div class=\"belldateandtime\">\n                            ").concat(bellData.dateAndTime, "\n                        </div>\n                    </div>\n                \n        ");
+        section.appendChild(bellitem);
+    });
+}
+var cardDat = [
+    {
+        img: "./images/imageMask.png",
+        topic: "Acceleration",
+        subject: "Physics",
+        grade: "7",
+        grade_plus: "+2",
+        units: 4,
+        lessons: 18,
+        topics: 24,
+        teacher_class: "Mr. Frank's Class B",
+        no_of_students: 50,
+        date_of_class: "21-Jan-2020 - 21-Aug-2020",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: true,
+        grade_submission: true,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-1.png",
+        topic: "Displacement, Velocity and Speed",
+        subject: "Physics 2",
+        grade: "6",
+        grade_plus: "+3",
+        units: 2,
+        lessons: 15,
+        topics: 20,
+        teacher_class: "",
+        no_of_students: null,
+        date_of_class: "",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: false,
+        grade_submission: false,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-3.png",
+        topic: "Introduction to Biology: Micro organisms and how they affec...",
+        subject: "Biology",
+        grade: "4",
+        grade_plus: "+1",
+        units: 5,
+        lessons: 16,
+        topics: 22,
+        teacher_class: "All Classes",
+        no_of_students: 300,
+        date_of_class: "",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: false,
+        grade_submission: false,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-2.png",
+        topic: "Introduction to High School Mathematics",
+        subject: "Mathematics",
+        grade: "8",
+        grade_plus: "+3",
+        units: null,
+        lessons: null,
+        topics: null,
+        teacher_class: "Mr. Frank's Class A",
+        no_of_students: 44,
+        date_of_class: "14-Oct-2019 - 20-Oct-2020",
+        is_favourite: false,
+        isExpired: true,
+        preview: true,
+        manage_course: true,
+        grade_submission: true,
+        reports: true
+    }
+];
+var announcement = [
+    {
+        pa: "Wilson Kumar",
+        checked: true,
+        content: "No classes will be held on 21st Nov",
+        course: "Mathematics 101",
+        filesAttached: 2,
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    },
+    {
+        pa: "Samson White",
+        checked: true,
+        content: "Guest lecture on Geometry on 20th September",
+        course: "",
+        filesAttached: 2,
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    },
+    {
+        pa: "Wilson Kumar",
+        checked: false,
+        content: "Additional course materials available on request",
+        course: "Mathematics 101",
+        filesAttached: 0,
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    },
+    {
+        pa: "Wilson Kumar",
+        checked: true,
+        content: "No classes will be held on 25th Dec",
+        course: "",
+        filesAttached: 0,
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    }
+];
+var belldata = [
+    {
+        checked: true,
+        content: "License for Introduction to Algebra has been assigned to your school",
+        course: "Mathematics 101",
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    },
+    {
+        checked: false,
+        content: "Lesson 3 Practice Worksheet overdue for Amy Santiago",
+        course: "Advanced Mathematics",
+        dateAndTime: "15-Sep-2018 at 05:21 pm"
+    },
+    {
+        checked: false,
+        content: "23 new students created",
+        course: "",
+        dateAndTime: "14-Sep-2018 at 01:21 pm"
+    },
+    {
+        checked: false,
+        content: "15 submissions ready for evaluation (Class: Basics of Algebra)",
+        course: "Basics of Algebra",
+        dateAndTime: "13-Sep-2018 at 01:15 pm"
+    },
+    {
+        checked: true,
+        content: "License for Basic Concepts in Geometry has been assigned to your school",
+        course: "Mathematics 101",
+        dateAndTime: "15-Sep-2018 at 07:21 pm"
+    }
+];
+populateCard(cardDat);
+populateannouncement(announcement);
+populatebelll(belldata);
+var hamburgerr = document.getElementById('hamburger');
+var hamburgermenuu = document.getElementsByClassName('hiddenhamburger');
+if (hamburgerr && hamburgermenuu.length > 0) {
+    hamburgerr.addEventListener('mouseover', function (e) {
+        hamburgermenuu[0].classList.add('ablehamburger');
+    });
+    hamburgerr.addEventListener('mouseout', function (e) {
+        hamburgermenuu[0].classList.remove('ablehamburger');
+    });
+}
+else {
+    console.error("Element(s) not found: hamburger or hiddenhamburger");
+}
+function handlebell() {
+    var bell = document.getElementById('bell');
+    var bellimg = 'green';
+    var belldiv = document.querySelector('.belldiv');
+    if (!bell || !belldiv) {
+        return "<h1>no data to show</h1>";
+    }
+    bell.addEventListener('click', function () {
+        if (bellimg == 'green') {
+            bell.innerHTML = '<img src="./icons/alerts-clicked.svg"><span class="badge">1</span>';
+            bellimg = 'white';
+            belldiv.style.display = 'flex';
+        }
+        else {
+            bell.innerHTML = '<img src="./icons/alerts.svg"><span class="badge">1</span>';
+            bellimg = 'green';
+            belldiv.style.display = 'none';
+        }
+    });
+}
+handlebell();
+function handleann() {
+    var ann = document.getElementById('announcements');
+    var annimg = 'green';
+    var anndiv = document.querySelector('.anndiv');
+    if (!ann || !anndiv) {
+        return "<h1>no data to show</h1>";
+    }
+    ann.addEventListener('click', function () {
+        if (annimg == 'green') {
+            ann.innerHTML = '<img src="./icons/announcement-clicked.svg"><span class="badge">1</span>';
+            annimg = 'white';
+            // anndiv.style.display=''
+            anndiv.style.display = 'flex';
+        }
+        else {
+            ann.innerHTML = '<img src="./icons/announcements.svg"><span class="badgeann">1</span>';
+            annimg = 'green';
+            anndiv.style.display = 'none';
+        }
+    });
+}
+handleann();
+console.log('hi');
+var navv = document.getElementsByTagName('li');
+var _loop_1 = function (i) {
+    navv[i].addEventListener('click', function (e) {
+        for (var j = 0; j < navv.length; j++) {
+            navv[j].setAttribute('class', 'navitem');
+        }
+        navv[i].setAttribute('class', 'selected-navitem');
+    });
+};
+for (var i = 0; 1 <= navv.length; i++) {
+    _loop_1(i);
+}
---
+++ Task5/TS/screen2.ts
@@ -0,0 +1,419 @@
+interface card {
+        img: string,
+        topic: string,
+        subject: string,
+        grade: string,
+        grade_plus: string,
+        units: number | null,
+        lessons: number | null,
+        topics: number |null,
+        teacher_class: string,
+        no_of_students: number | null,
+        date_of_class: string,
+        is_favourite: boolean,
+        isExpired: boolean,
+        preview: boolean,
+        manage_course: boolean,
+        grade_submission: boolean,
+        reports:boolean
+}
+
+
+
+interface announcementdata {
+    pa: string,
+    checked: boolean,
+    content: string,
+    course: string,
+    filesAttached: number,
+    dateAndTime: string
+}
+
+
+interface bell {
+    checked: boolean,
+    content: string,
+    course: string,
+    dateAndTime: string
+}
+
+function populateCard(cardDataArray : card[]): void | string {
+    const section = document.querySelector('.container');
+    
+    if(!section ){
+      return `<h1> nothing</h1>`
+    }
+    cardDataArray.forEach(cardData => {
+        const card = document.createElement('section');
+        card.className = 'section3';
+        
+        card.innerHTML = `
+            ${cardData.isExpired ? `<div class="expired"><p>EXPIRED</p></div>` : ''}
+            <div class='card'>
+                
+                <div class="courseimage">
+                    <img src="${cardData.img}">
+                </div>
+                <div class="coursedetails">
+                    <div class="sec1">
+                        <p>${cardData.topic}</p>
+                        ${cardData.is_favourite ? `<img src="./icons/favourite.svg">` :`<img src="./icons/notFavourite.svg">`}
+                        
+                    </div>
+                    <div class="sec2">
+                        <p>${cardData.subject}</p>
+                        <p>|</p>
+                        <p>Grade<span> ${cardData.grade}</span><span style="color : green;"> ${cardData.grade_plus}</span></p>
+                    </div>
+                    <div class="sec3">
+                    ${cardData.units ? `<p><span><b>${cardData.units}</b></span> Units</p>` : ''}
+                    ${cardData.lessons ? `<p><span><b>${cardData.lessons}</b></span> Lessons</p>` : ''}
+                    ${cardData.topics ? `<p><span><b>${cardData.topics}</b></span> Topics</p>` : ''}
+                    </div>
+
+                    <div class="sec4">
+                        <select class="teachername" id="teachername">
+                        
+                            ${cardData.teacher_class ? `<option  selected>${cardData.teacher_class  }</option>` : `<option  selected>No Classes</option>`}
+                        </select>
+                    </div>
+                    <div class="sec5">
+                        ${cardData.no_of_students ?`<p>${cardData.no_of_students} Students</p>`: ''}
+                        ${cardData.date_of_class ? `<p>|</p>` : ''}
+                        ${cardData.date_of_class ? `<p>${cardData.date_of_class}</p>` : ''}
+                    </div>
+                </div>
+            </div>
+            
+        `;
+
+        const actions = document.createElement('div');
+        actions.className = 'actions';
+        actions.innerHTML = `
+            <div class='div1' style="opacity: ${cardData.preview ? "1" : "0.4"}">
+                <img src="./icons/preview.svg">
+            </div>
+            <div  style="opacity: ${cardData.manage_course ? "1" : "0.4"}">
+                <img src="./icons/manage course.svg">
+            </div>
+            <div  style="opacity: ${cardData.grade_submission ? "1" : "0.4"}">
+                <img src="./icons/grade submissions.svg">
+            </div>
+            <div class='div4' style="opacity: ${cardData.reports ? "1" : "0.4"}">
+                <img src="./icons/reports.svg">
+            </div>
+        `;
+
+        section?.appendChild(card);
+        card.appendChild(actions);
+    });
+}
+
+function populateannouncement(annDataArray : announcementdata[]) {
+    const section = document.querySelector('.announcementitems');
+    if (!section){
+      return `<h1> no section</h1>`
+    }
+    annDataArray.forEach(annData => {
+        const annitem = document.createElement('div');
+        annitem.className = 'annitem'; // Fixed variable name from 'item' to 'annitem'
+        annitem.style.backgroundColor = !annData.checked ?  "#FFFFEE" : 'white'
+
+        annitem.innerHTML = `
+            <div class="annsec1">
+                <div class="panadname">
+                    <span class="pa">PA:</span><span class="name">${annData.pa}</span>
+                </div>
+                <div class="checkimage">
+                    ${annData.checked ? `<img src="./icons/checkbox-checked.svg">` : `<img src="./icons/checkbox-unchecked.svg">`}
+                </div>
+            </div>
+            <div class="annsec2">
+                <div class="content">
+                    <p>${annData.content}</p>
+                </div>
+            </div>
+            <div class="annsec3">
+                <div class="courseinann">
+                    ${!annData.course ? '': `<span>Course :</span> <span class="coursename">${annData.course}</span>`}
+                    
+                </div>
+            </div>
+            <div class="annsec4">
+                <div class="files">
+                    ${annData.filesAttached != 0 ?`<span>${annData.filesAttached}</span><span> Files attached</span>` : '' }
+                    
+                </div>
+                <div class="dateandtime">
+                    ${annData.dateAndTime}
+                </div>
+            </div>
+        `;
+
+        section.appendChild(annitem);
+    });
+}
+
+function populatebelll(bellDataArray : bell[]) {
+    const section = document.querySelector('.bellitems');
+    if (!section){
+      return `<h1>no data to show</h1>`
+    }
+    bellDataArray.forEach(bellData => {
+        const bellitem = document.createElement('div');
+        bellitem.className = 'bellitem'; // Fixed variable name from 'item' to 'bellitem'
+        bellitem.style.backgroundColor = !bellData.checked ?  "#FFFFEE" : 'white'
+
+        bellitem.innerHTML = `
+                
+                    <div class="bellsec1">
+                        <div class="bellcontent">
+                            <p>${bellData.content}</p>
+                        </div>
+                        <div class="bellcheckimage">
+                            ${bellData.checked ? `<img src="./icons/checkbox-checked.svg">` : `<img src="./icons/minus.png">`}
+                        </div>
+                    </div>
+                    <div class="bellsec2">
+                        <div class="bellcourse">
+                            ${!bellData.course ? '': `<span>Course :</span> <span class="coursename">${bellData.course}</span>`}
+                        </div>
+                    </div>
+                    <div class="bellsec3">
+                        <div class="belldateandtime">
+                            ${bellData.dateAndTime}
+                        </div>
+                    </div>
+                
+        `;
+
+        section.appendChild(bellitem);
+    });
+}
+
+const cardDat : card[] = [
+    {
+        img: "./images/imageMask.png",
+        topic: "Acceleration",
+        subject: "Physics",
+        grade: "7",
+        grade_plus: "+2",
+        units: 4,
+        lessons: 18,
+        topics: 24,
+        teacher_class: "Mr. Frank's Class B",
+        no_of_students: 50,
+        date_of_class: "21-Jan-2020 - 21-Aug-2020",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: true,
+        grade_submission: true,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-1.png",
+        topic: "Displacement, Velocity and Speed",
+        subject: "Physics 2",
+        grade: "6",
+        grade_plus: "+3",
+        units: 2,
+        lessons: 15,
+        topics: 20,
+        teacher_class: "",
+        no_of_students: null,
+        date_of_class: "",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: false,
+        grade_submission: false,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-3.png",
+        topic: "Introduction to Biology: Micro organisms and how they affec...",
+        subject: "Biology",
+        grade: "4",
+        grade_plus: "+1",
+        units: 5,
+        lessons: 16,
+        topics: 22,
+        teacher_class: "All Classes",
+        no_of_students: 300,
+        date_of_class: "",
+        is_favourite: true,
+        isExpired: false,
+        preview: true,
+        manage_course: false,
+        grade_submission: false,
+        reports: true
+    },
+    {
+        img: "./images/imageMask-2.png",
+        topic: "Introduction to High School Mathematics",
+        subject: "Mathematics",
+        grade: "8",
+        grade_plus: "+3",
+        units: null,
+        lessons: null,
+        topics: null,
+        teacher_class: "Mr. Frank's Class A",
+        no_of_students: 44,
+        date_of_class: "14-Oct-2019 - 20-Oct-2020",
+        is_favourite: false,
+        isExpired: true,
+        preview: true,
+        manage_course: true,
+        grade_submission: true,
+        reports: true
+    }
+];
+
+const announcement : announcementdata[] = [
+  {
+    pa: "Wilson Kumar",
+    checked: true,
+    content: "No classes will be held on 21st Nov",
+    course: "Mathematics 101",
+    filesAttached: 2,
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  },
+  {
+    pa: "Samson White",
+    checked: true,
+    content: "Guest lecture on Geometry on 20th September",
+    course: "",
+    filesAttached: 2,
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  },
+  {
+    pa: "Wilson Kumar",
+    checked: false,
+    content: "Additional course materials available on request",
+    course: "Mathematics 101",
+    filesAttached: 0,
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  },
+  {
+    pa: "Wilson Kumar",
+    checked: true,
+    content: "No classes will be held on 25th Dec",
+    course: "",
+    filesAttached: 0,
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  }
+]
+
+const belldata = [
+  {
+    checked: true,
+    content: "License for Introduction to Algebra has been assigned to your school",
+    course: "Mathematics 101",
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  },
+  {
+    checked: false,
+    content: "Lesson 3 Practice Worksheet overdue for Amy Santiago",
+    course: "Advanced Mathematics",
+    dateAndTime: "15-Sep-2018 at 05:21 pm"
+  },
+  {
+    checked: false,
+    content: "23 new students created",
+    course: "",
+    dateAndTime: "14-Sep-2018 at 01:21 pm"
+  },
+  {
+    checked: false,
+    content: "15 submissions ready for evaluation (Class: Basics of Algebra)",
+    course: "Basics of Algebra",
+    dateAndTime: "13-Sep-2018 at 01:15 pm"
+  },
+  {
+    checked: true,
+    content: "License for Basic Concepts in Geometry has been assigned to your school",
+    course: "Mathematics 101",
+    dateAndTime: "15-Sep-2018 at 07:21 pm"
+  }
+]
+
+populateCard(cardDat)
+populateannouncement(announcement)
+populatebelll(belldata)
+
+let hamburgerr = document.getElementById('hamburger');
+let hamburgermenuu = document.getElementsByClassName('hiddenhamburger');
+
+if (hamburgerr && hamburgermenuu.length > 0) {
+  hamburgerr.addEventListener('mouseover', (e) => {
+    hamburgermenuu[0].classList.add('ablehamburger');
+    
+
+  });
+
+  hamburgerr.addEventListener('mouseout', (e) => {
+    hamburgermenuu[0].classList.remove('ablehamburger');
+  });
+
+} else {
+  console.error("Element(s) not found: hamburger or hiddenhamburger");
+}
+
+function handlebell(){
+    let bell = document.getElementById('bell')
+    let bellimg='green'
+    let belldiv = document.querySelector<HTMLElement>('.belldiv')
+    if(!bell || !belldiv){
+        return`<h1>no data to show</h1>`
+    }
+    bell.addEventListener('click',()=>{
+        if (bellimg=='green'){
+            bell.innerHTML='<img src="./icons/alerts-clicked.svg"><span class="badge">1</span>';
+            bellimg= 'white';
+            belldiv.style.display='flex'
+        }
+        else{
+            bell.innerHTML='<img src="./icons/alerts.svg"><span class="badge">1</span>';
+            bellimg='green'
+            belldiv.style.display='none'
+        }
+    })
+}
+handlebell()
+
+function handleann(){
+    let ann = document.getElementById('announcements')
+    let annimg='green'
+    let anndiv = document.querySelector<HTMLElement>('.anndiv')
+    if(!ann || !anndiv){
+        return `<h1>no data to show</h1>`
+    }
+
+    ann.addEventListener('click',()=>{
+        if (annimg=='green'){
+            ann.innerHTML='<img src="./icons/announcement-clicked.svg"><span class="badge">1</span>';
+            annimg= 'white';
+            // anndiv.style.display=''
+            anndiv.style.display='flex'
+        }
+        else{
+            ann.innerHTML='<img src="./icons/announcements.svg"><span class="badgeann">1</span>';
+            annimg='green'
+            anndiv.style.display='none'
+            
+        }
+    })
+}
+handleann()
+console.log('hi')
+let navv = document.getElementsByTagName('li')
+for(let i=0;1<=navv.length;i++){
+    navv[i].addEventListener('click',(e)=>{
+    for(let j=0;j<navv.length;j++){
+        navv[j].setAttribute('class','navitem')
+    }
+
+    navv[i].setAttribute('class','selected-navitem')
+    })
+}
